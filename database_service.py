7e8f4db44781474d5d1778d994f25d86ed8af4f0
jleo32 <justin.s.leo32@gmail.com>
2021-04-04 21:21:52 -0400

    Updated POST method in item endpoint to allow insertion of new item to database

--- database_service.py
+++ database_service.py
@@ -22,7 +22,10 @@ def init_db():
 def connect_db():
     """
     Created connection to the database for app context
-    :return:
+
+    Database factory: db.row_factory = sqlite3.Row
+
+    :return: Connection to local sqlite database
     """
     try:
         db = sqlite3.connect('fearless.db')
@@ -48,13 +51,33 @@ def close_db():
 def get_all_items():
     cursor = g.db.cursor()
 
-    cursor.execute("SELECT * FROM items")
+    try:
+        cursor.execute('SELECT * FROM items;')
+    except Exception as e:
+        print('Unable to retrieve items: ' + str(e))
+        return []
 
     return cursor.fetchall()
 
 
-def add_item(item):
-    pass
+def add_item(item_name):
+    """
+    Adds a new item with `item_name` to the `items` table
+
+    :param item_name: String of name to add
+
+    :return: True is insertion is successful
+    """
+    connection = g.db
+    cursor = g.db.cursor()
+
+    try:
+        cursor.execute(f"INSERT INTO items('name') VALUES ('{item_name}');")
+        connection.commit()
+    except Exception as e:
+        print('Unable to add item: ' + str(e))
+        return False
+    return True
 
 
 def get_item_by_id(item_id):
